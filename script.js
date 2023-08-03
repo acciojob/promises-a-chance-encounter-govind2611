ec08b379aab6b8338c098410b53424886ae44e09
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-08-03 09:33:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+/ Generate five <div> elements with IDs
+for (let i = 1; i <= 5; i++) {
+  const div = document.createElement('div');
+  div.id = `div-${i}`;
+  document.body.appendChild(div);
+}
+// Check that there are five <div> elements with IDs
+for (let i = 1; i <= 5; i++) {
+  cy.get(`#div-${i}`).should('exist');
+}
